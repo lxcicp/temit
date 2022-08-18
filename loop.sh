75667c60031100b3aecb27632766f76d2a140208
lxcicp <zimuchunzi0508@gmail.com>
2022-08-18 13:14:42 +0800

    git auto commit

--- loop.sh
+++ loop.sh
@@ -5,7 +5,7 @@ fi
 for i in $(seq 1 $1)
 do 
    `sh push.sh`
-   time=`date +'%G-%m-%d %H:%M:%S' -d '-1 days'`
+   time=`date +'%G-%m-%d %H:%M:%S' -d '-120 days'`
    timedatectl set-time "$time"
 done
 
